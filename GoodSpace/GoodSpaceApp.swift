04f2fad84162f75512dc0c32cfc6db857928993b
Minseok <staralstjr@naver.com>
2025-07-15 15:53:16 +0900

    Initial Commit

---
+++ GoodSpace/GoodSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodSpaceApp.swift
+//  GoodSpace
+//
+//  Created by minseok on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GoodSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
